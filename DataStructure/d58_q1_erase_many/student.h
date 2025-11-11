98eefadfa428a82e57b3bbb5d85ad1a14b5d0691
kaikub <thunwa.pungtam@gmail.com>
2025-11-11 20:27:13 +0700

    Add range_insert implementation scaffold and update settings
    Added new files for the d59_q1_range_insert problem, including main.cpp, student.h, vector.h, and project file. Updated .vscode/settings.json to improve C++ file associations. Implemented erase_many in student.h for d58_q1_erase_many using reverse-sorted indices and erase. The insert function for range insertion is scaffolded for future implementation.

--- DataStructure/d58_q1_erase_many/student.h
+++ DataStructure/d58_q1_erase_many/student.h
@@ -1,10 +1,14 @@
 #ifndef __STUDENT_H_
 #define __STUDENT_H_
-
+#include <algorithm>
 
 template <typename T>
 void CP::vector<T>::erase_many(const std::vector<int> &pos) {
-  //write your code here
+  std::vector<int> m = pos;
+  std::sort(m.begin(),m.end(),std::greater<int>());
+  for(int i:m){
+    erase(begin() + i);
+  }
 }
 
 #endif
