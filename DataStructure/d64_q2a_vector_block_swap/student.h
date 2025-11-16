656e1499555c588b6c29faacf8e58d244a4c9aad
kaikub <thunwa.pungtam@gmail.com>
2025-11-16 15:55:36 +0700

    Update student.h

--- DataStructure/d64_q2a_vector_block_swap/student.h
+++ DataStructure/d64_q2a_vector_block_swap/student.h
@@ -3,7 +3,18 @@
 
 template <typename T>
 bool CP::vector<T>::block_swap(iterator a, iterator b, size_t m) {
-  //write your code here
+  if(m==0) return false;
+  size_t Size = end() - begin();  
+  size_t A = a -begin();
+  size_t  B = b-begin();
+  if (A >= Size || B >= Size || A + m > Size || B + m > Size || (A + m > B && B + m > A)) return false;
+
+  for(size_t i = 0;i<m;++i){
+    T temp = mData[A+i];
+    mData[A+i] = mData[B+i];
+    mData[B+i] = temp;
+  }
+  return true;
 }
 
 #endif
